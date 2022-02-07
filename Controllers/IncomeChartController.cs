bc2d9414665a316b43d89bb98b2fd506cd650ee3
HyebeenEmma <parkem0219@gmail.com>
2022-02-06 22:00:04 -0500

    live bar graph

--- Controllers/IncomeChartController.cs
+++ Controllers/IncomeChartController.cs
@@ -5,25 +5,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using PinewoodGrow.Models;
+using PinewoodGrow.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace PinewoodGrow.Controllers
 {
     public class IncomeChartController : Controller
     {
-		// GET: Home
-		public ActionResult Index()
+		private readonly GROWContext _context;
+
+		public IncomeChartController(GROWContext context)
 		{
+			_context = context;
+		}
+
+		// GET: Home
+		public async Task <IActionResult> Index()
+		{
+
+			var member = from m in _context.Members
+						 select m;
+
+			//sort income by datapoint
+			double mInc1 = member.ToList().Count(m => m.Income < 5000);
+			double mInc2 = member.ToList().Count(m => (m.Income <= 9999) && (m.Income >= 5000));
+			double mInc3 = member.ToList().Count(m => (m.Income <= 14999) && (m.Income >= 10000));
+			double mInc4 = member.ToList().Count(m => (m.Income <= 19999) && (m.Income >= 15000));
+			double mInc5 = member.ToList().Count(m => (m.Income <= 24999) && (m.Income >= 20000));
+			double mInc6 = member.ToList().Count(m => m.Income >= 25000);
+			
+
+
 			List<DataPoint> dataPoints = new List<DataPoint>();
 
-			dataPoints.Add(new DataPoint("Under $5000", 16));
-			dataPoints.Add(new DataPoint("$5000 to $9999", 13));
-			dataPoints.Add(new DataPoint("$10000 to $14999", 20));
-			dataPoints.Add(new DataPoint("$15000 to $19999", 7));
-			dataPoints.Add(new DataPoint("$20000 to $24999", 5));
-			dataPoints.Add(new DataPoint("$25000 or more", 12));
+			dataPoints.Add(new DataPoint("Under $5000", mInc1));
+			dataPoints.Add(new DataPoint("$5000 to $9999", mInc2));
+			dataPoints.Add(new DataPoint("$10000 to $14999", mInc3));
+			dataPoints.Add(new DataPoint("$15000 to $19999", mInc4));
+			dataPoints.Add(new DataPoint("$20000 to $24999", mInc5));
+			dataPoints.Add(new DataPoint("$25000 or more", mInc6));
 
 
-			ViewBag.DataPoints = JsonConvert.SerializeObject(dataPoints);
+            ViewData["graphData"] = JsonConvert.SerializeObject(dataPoints);
+            ViewData["tableData"] = dataPoints;
 
 			return View();
 		}
