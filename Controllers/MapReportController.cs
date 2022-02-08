baa02a6d58438afc928b7be7d9451fc038b8db39
PineappleSoda <anaanAndrews@gmail.com>
2022-02-08 10:39:02 -0500

    Updated Map Report

--- Controllers/MapReportController.cs
+++ Controllers/MapReportController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
@@ -26,9 +27,24 @@ namespace PinewoodGrow.Controllers
 
             //Converts List of Addresses to map markers exudes all entries that do not have enter lat/longs
 
-            Markers = _context.Addresses
+            /*Markers = _context.Addresses
                 .Where(a => a.Latitude != 0 && a.Longitude != 0)
-                .Select(a => new MapMarker() { Address = a.FullAddress, Lat = (double)a.Latitude, Lng = (double)a.Longitude }).ToList();
+                .Select(a => new MapMarker() { Address = a.FullAddress, Lat = (double)a.Latitude, Lng = (double)a.Longitude }).ToList();*/
+
+            Markers = _context.Households.Include(a => a.Address)
+                .Where(a => a.Address.Latitude != 0 && a.Address.Longitude != 0)
+                .Select(a=> new MapMarker()
+                {
+                    Address = a.Address.FullAddress,
+                    Lat = (double)a.Address.Latitude, 
+                    Lng = (double)a.Address.Longitude, 
+                    Income = (double)a.HouseIncome,
+                    Color = GetColor((double)a.HouseIncome),
+                    FamilySize = a.FamilySize,
+                    Category = GetCategory((double)a.HouseIncome)
+
+                }).ToList();
+
 
             ViewData["Markers"] = Markers;
 
@@ -36,5 +52,29 @@ namespace PinewoodGrow.Controllers
 
             return View();
         }
+
+        private static string GetColor(double income)
+        {
+            return income switch
+            {
+                var i when i <= 5000 => "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
+                var i when i <= 10000 => "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+                var i when i <= 15000 => "http://maps.google.com/mapfiles/ms/icons/purple-dot.png",
+                var i when i <= 20000 => "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png",
+                _ => "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
+            };
+        }
+        private static string GetCategory(double income)
+        {
+            return income switch
+            {
+                var i when i <= 5000 => "Lowest",
+                var i when i <= 10000 => "Low",
+                var i when i <= 15000 => "Mid",
+                var i when i <= 20000 => "High",
+                _ => "Top"
+            };
+        }
     }
+
 }
--- ViewModels/MapMarker.cs
+++ ViewModels/MapMarker.cs
@@ -7,10 +7,16 @@ namespace PinewoodGrow.ViewModels
 {
     public class MapMarker
     {
+        public int ID { get; set; }
         public double Lat { get; set; }
         public double Lng { get; set; }
-
         public string Address { get; set; }
 
+        public double Income { get; set; }
+
+        public string Color { get; set; }
+
+        public int FamilySize { get; set; }
+        public string Category { get; set; }
     }
 }
