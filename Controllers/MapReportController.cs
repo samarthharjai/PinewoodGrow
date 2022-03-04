911a16a33138b8b0cc8e5352df9d882000f635a6
PineappleSoda <anaanAndrews@gmail.com>
2022-03-04 17:37:27 -0500

    Update to Map Report popup

--- Controllers/MapReportController.cs
+++ Controllers/MapReportController.cs
@@ -31,28 +31,10 @@ namespace PinewoodGrow.Controllers
             //Converts List of Addresses to map markers exudes all entries that do not have enter lat/longs
 
             Markers = _context.Households.Include(a => a.Address)
-                .ThenInclude(t=> t.TravelDetail)
-                .ThenInclude(t=> t.GroceryStore)
+                .ThenInclude(t => t.TravelDetail)
+                .ThenInclude(t => t.GroceryStore)
                 .Where(a => a.Address.Latitude != 0 && a.Address.Longitude != 0)
-                .Select(a=> new MapMarker()
-                {
-                    Address = a.Address.FullAddress,
-                    Lat = (double)a.Address.Latitude, 
-                    Lng = (double)a.Address.Longitude, 
-                    Income = a.HouseIncome,
-                    Color = GetColor(a.HouseIncome),
-                    FamilySize = a.FamilySize,
-                    Category = GetCategory(a.HouseIncome),
-                    GrowDistance = a.Address.TravelDetail.GrowDistance,
-                    GrowDrive = a.Address.TravelDetail.GrowDrive,
-                    GrowBike= a.Address.TravelDetail.GrowBike,
-                    GrowWalk = a.Address.TravelDetail.GrowWalk,
-                    GroceryName = a.Address.TravelDetail.GroceryStore.Name,
-                    GroceryDistance = a.Address.TravelDetail.GroceryDistance,
-                    GroceryDrive = a.Address.TravelDetail.GroceryDrive,
-                    GroceryBike = a.Address.TravelDetail.GroceryBike,
-                    GroceryWalk = a.Address.TravelDetail.GroceryWalk,
-                }).ToList();
+                .Select(a => new MapMarker(a, a.HouseIncome)).ToList();
 
 
 
@@ -63,31 +45,9 @@ namespace PinewoodGrow.Controllers
             return View();
         }
 
-        private static string GetColor(double income)
-        {
-            return income switch
-            {
-                var i when i <= 5000 => "http://maps.google.com/mapfiles/ms/icons/blue-dot.png",
-                var i when i <= 10000 => "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
-                var i when i <= 15000 => "http://maps.google.com/mapfiles/ms/icons/purple-dot.png",
-                var i when i <= 20000 => "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png",
-                _ => "http://maps.google.com/mapfiles/ms/icons/green-dot.png"
-            };
-        }
-        private static string GetCategory(double income)
-        {
-            return income switch
-            {
-                var i when i <= 5000 => "Lowest",
-                var i when i <= 10000 => "Low",
-                var i when i <= 15000 => "Mid",
-                var i when i <= 20000 => "High",
-                _ => "Top"
-            };
-        }
     }
 
-}
+}//http://maps.google.com/mapfiles/ms/icons/purple-dot.png
 
 /*if (!_context.TravelDetails.Any())
 {
--- ViewModels/MapMarker.cs
+++ ViewModels/MapMarker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PinewoodGrow.Models;
 
 namespace PinewoodGrow.ViewModels
 {
@@ -11,9 +12,9 @@ namespace PinewoodGrow.ViewModels
         public double Lat { get; set; }
         public double Lng { get; set; }
         public string Address { get; set; }
-
+        public string Icon { get; set; }
         public double Income { get; set; }
-
+        public string FamilyName { get; set; }
         public string Color { get; set; }
 
         public int FamilySize { get; set; }
@@ -31,6 +32,37 @@ namespace PinewoodGrow.ViewModels
         public double GrowBike { get; set; }
         public double GrowWalk { get; set; }
 
+        public MapMarker(Household a, double income)
+        {
+            ID = a.ID;
+            (Icon, Color, Category) = GetColor(income);
+            Address = a.Address.FullAddress;
+            if (a.Address.Latitude != null) Lat = (double)a.Address.Latitude;
+            if (a.Address.Longitude != null) Lng = (double)a.Address.Longitude;
+            Income = income;
+            FamilySize = a.FamilySize;
+            FamilyName = a.FamilyName;
+            GrowDistance = a.Address.TravelDetail.GrowDistance;
+            GrowDrive = a.Address.TravelDetail.GrowDrive;
+            GrowBike = a.Address.TravelDetail.GrowBike;
+            GrowWalk = a.Address.TravelDetail.GrowWalk;
+            GroceryName = a.Address.TravelDetail.GroceryStore.Name;
+            GroceryDistance = a.Address.TravelDetail.GroceryDistance;
+            GroceryDrive = a.Address.TravelDetail.GroceryDrive;
+            GroceryBike = a.Address.TravelDetail.GroceryBike;
+            GroceryWalk = a.Address.TravelDetail.GroceryWalk;
+        }
+        internal Tuple<string, string, string> GetColor(double income)
+        {
+            return income switch
+            {
+                var i when i <= 5000 => Tuple.Create("http://maps.google.com/mapfiles/ms/icons/blue-dot.png", "#6a92fc", "Lowest"),
+                var i when i <= 10000 => Tuple.Create("http://maps.google.com/mapfiles/ms/icons/red-dot.png", "#fd7567", "Low"),
+                var i when i <= 15000 => Tuple.Create("http://maps.google.com/mapfiles/ms/icons/purple-dot.png", "#8e67fd", "Mid"),
+                var i when i <= 20000 => Tuple.Create("http://maps.google.com/mapfiles/ms/icons/yellow-dot.png", "#fdf569", "High"),
+                _ => Tuple.Create("http://maps.google.com/mapfiles/ms/icons/green-dot.png", "#00e64d", "Top"),
 
+            };
+        }
     }
 }
