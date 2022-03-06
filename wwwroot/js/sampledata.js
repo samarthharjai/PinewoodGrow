2d9131bb922900dbc4a1fb2ba6bb78b2274d3a69
HyebeenEmma <parkem0219@gmail.com>
2022-03-05 22:15:49 -0500

    add sample data button, delete pre filled data values in member, household create view

---
+++ wwwroot/js/sampledata.js
@@ -0,0 +1,21 @@
+﻿
+
+document.getElementById("btnSampleData").addEventListener("click", function () {
+    
+    document.getElementById("txtFirstName").value = "Jone";
+    document.getElementById("txtLastName").value = "Doe";
+    document.getElementById("txtAge").value = "2001-02-08";
+    document.getElementById("ddlGender").value = "1";
+    document.getElementById("txtPhone").value = "6367727345";
+    document.getElementById("txtEmail").value = "sample@gmail.com";
+    document.getElementById("txtIncome").value = "500";
+    document.getElementById("txtComplete").value = "2";
+    document.getElementById("ddlHousehold").value = "1";
+    document.getElementById("chkIllnesses").value = "1";
+    document.getElementById("chkSituations").value = "1";
+    document.getElementById("autocomplete").value = "5951 Main St, Niagara Falls, ON";
+    document.getElementById("postal").value = "L2G 5Z7";
+    document.getElementById("txtDependants").value = "2";
+    document.getElementById("txtFamilyName").value = "Doe";
+
+})
