898cc640e9227e5da8c63ebfa24d224260e8a69a
HyebeenEmma <parkem0219@gmail.com>
2022-03-05 22:50:51 -0500

    add script bottom of the create view

--- wwwroot/js/sampledata.js
+++ wwwroot/js/sampledata.js
@@ -1,7 +1,6 @@
 ﻿
 
 document.getElementById("btnSampleData").addEventListener("click", function () {
-    
     document.getElementById("txtFirstName").value = "Jone";
     document.getElementById("txtLastName").value = "Doe";
     document.getElementById("txtAge").value = "2001-02-08";
@@ -13,9 +12,22 @@ document.getElementById("btnSampleData").addEventListener("click", function () {
     document.getElementById("ddlHousehold").value = "1";
     document.getElementById("chkIllnesses").value = "1";
     document.getElementById("chkSituations").value = "1";
+
+})
+
+document.getElementById("btnSampleDataHouse").addEventListener("click", function () {
     document.getElementById("autocomplete").value = "5951 Main St, Niagara Falls, ON";
     document.getElementById("postal").value = "L2G 5Z7";
     document.getElementById("txtDependants").value = "2";
     document.getElementById("txtFamilyName").value = "Doe";
-
+    document.getElementById("txtFirstName").value = "Jane";
+    document.getElementById("txtLastName").value = "Doe";
+    document.getElementById("txtAge").value = "1984-02-04";
+    document.getElementById("ddlGender").value = "2";
+    document.getElementById("txtPhone").value = "6367727614";
+    document.getElementById("txtEmail").value = "sample@gmail.com";
+    document.getElementById("txtIncome").value = "1600";
+    document.getElementById("txtComplete").value = "2";
+    document.getElementById("chkIllnesses").value = "2";
+    document.getElementById("chkSituations").value = "7";
 })
