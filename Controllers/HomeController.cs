903e720b73a3f4065d38b0b64dd8a1acaf041a40
AidanShaefer <75329585+AidanShaefer@users.noreply.github.com>
2022-04-04 11:07:56 -0400

    Removed redundant headers

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace PinewoodGrow.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
